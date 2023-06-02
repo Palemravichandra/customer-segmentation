e23b4c7302e5c1d84c74d0901485cf63144ad5e5
Palem Ravi chandra <123462175+Palemravichandra@users.noreply.github.com>
2023-06-02 19:22:17 +0530

    Update App2.py

--- App2.py
+++ App2.py
@@ -4,7 +4,7 @@ import pandas as pd
 import sklearn
 from xgboost import XGBClassifier
 
-st.title(':skyblue[CUSTOMER SEGMENTATION APP]')
+st.title(':blue[CUSTOMER SEGMENTATION APP]')
 file = open('customer_xgb.pkl','rb')
 model= pickle.load(file)
 col1,col2,col3=st.columns(3)
